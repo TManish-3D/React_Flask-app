a5aa8878891e9a4a429bf7bbbc7e0afe763b9a29
TManish-3D <mtimsina91@gmail.com>
2026-01-06 21:17:52 +0545

    react app

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  const goToFlask = () => {
+    window.location.href = "http://localhost:5000/hello";
+  };
+
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>React App</h1>
+      <button onClick={goToFlask}>
+        Go to Flask Route
+      </button>
+    </div>
+  );
+}
+
+export default App;
